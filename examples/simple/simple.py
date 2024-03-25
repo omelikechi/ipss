ea2ea860fb5ca9924b2145ec982b5444c79b88b0
Omar Melikechi <omar.melikechi@gmail.com>
2024-03-24 20:30:35 -0400

    simple.ipynb

--- examples/simple/simple.py
+++ examples/simple/simple.py
@@ -37,24 +37,28 @@ scores = result['scores']
 threshold = result['threshold']
 
 #--------------------------------
-# Visualize results
+# Plot results
 #--------------------------------
 # specify plot details
 color = ['dodgerblue' if value == 1 else 'gray' for value in truth]
 alpha = [1 if value == 1 else 0.5 for value in truth]
-point_size = [75 if value == 1 else 50 for value in truth]
+point_size = [100 if value == 1 else 75 for value in truth]
 
-fig, ax = plt.subplots(1, 2, figsize=(14,4))
+fig, ax = plt.subplots(1, 2, figsize=(14,5))
 
 # plot stability paths
 for j in range(p):
 	ax[0].plot(stability_paths[:,j], color=color[j], alpha=alpha[j])
+ax[0].set_title(f'Stability paths', fontsize=16)
 
 # plot scores
 noise = np.random.uniform(-.5, .5, size=p)
 ax[1].scatter(scores, noise, color=color, alpha=alpha, s=point_size)
-ax[1].axvline(threshold, linestyle='--', color='red')
+ax[1].axvline(threshold, linestyle='--', color='red', label=f'IPSS threshold')
+ax[1].legend(loc='best', fontsize=14)
 ax[1].set_ylim(-1,1)
+ax[1].set_title(f'IPSS scores', fontsize=16)
+
 plt.tight_layout()
 plt.show()
 
