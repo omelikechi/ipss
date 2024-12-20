2f77cd8ffe62a206a2ea1cb586dbb08dc5e59c22
Omar Melikechi <omar.melikechi@gmail.com>
2024-12-20 13:52:34 -0500

    version 1.0.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='ipss',
-	version='1.0.15',
+	version='1.0.16',
 	author='Omar Melikechi',
 	author_email='omar.melikechi@gmail.com',
 	packages=find_packages(),
